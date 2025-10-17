be9cfb0d919638bd01fcfc7ff036a0d54ece5249
vivek medorn <vivek@medorn.com>
2025-10-17 23:11:49 +0530

    testing

--- src/main/java/com/refrralApp/refrral/utility/Helper.java
+++ src/main/java/com/refrralApp/refrral/utility/Helper.java
@@ -8,12 +8,4 @@ import java.util.Iterator;
 @Component
 public class Helper {
 
-    public JsonNode merge(JsonNode original, JsonNode updates) {
-        Iterator<String> fieldNames = updates.fieldNames();
-        while (fieldNames.hasNext()) {
-            String fieldName = fieldNames.next();
-            ((ObjectNode) original).replace(fieldName, updates.get(fieldName));
-        }
-        return original;
     }
-}
